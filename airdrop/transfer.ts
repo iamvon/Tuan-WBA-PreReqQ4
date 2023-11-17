bff6e42c387bf68b2d05979377100bb523f67394
Tuan Pham Minh <tuanpmhd@gmail.com>
2023-11-17 16:18:54 +0700

    Empty devnet wallet into WBA wallet

--- airdrop/transfer.ts
+++ airdrop/transfer.ts
@@ -16,11 +16,14 @@ const connection = new Connection("https://api.devnet.solana.com");
 
 (async () => {
     try {
+        // Get balance of dev wallet
+        const balance = await connection.getBalance(from.publicKey)
+        // Create a test transaction to calculate fees
         const transaction = new Transaction().add(
             SystemProgram.transfer({
                 fromPubkey: from.publicKey,
                 toPubkey: to,
-                lamports: LAMPORTS_PER_SOL / 100,
+                lamports: balance,
             })
         );
         transaction.recentBlockhash = (await
@@ -28,6 +31,24 @@ const connection = new Connection("https://api.devnet.solana.com");
             connection.getLatestBlockhash('confirmed')).blockhash;
 
         transaction.feePayer = from.publicKey;
+        // Calculate exact fee rate to transfer entire SOL amount out of account minus fees
+
+        const fee = (await
+
+            connection.getFeeForMessage(transaction.compileMessage(),
+                'confirmed')).value || 0;
+
+        // Remove our transfer instruction to replace it
+        transaction.instructions.pop();
+        // Now add the instruction back with correct amount of lamports
+
+        transaction.add(
+            SystemProgram.transfer({
+                fromPubkey: from.publicKey,
+                toPubkey: to,
+                lamports: balance - fee,
+            })
+        );
         // Sign transaction, broadcast, and confirm
         const signature = await sendAndConfirmTransaction(
             connection,
@@ -35,7 +56,7 @@ const connection = new Connection("https://api.devnet.solana.com");
             [from]
         );
         console.log(`Success! Check out your TX here:
-    https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+    https://explorer.solana.com/tx/${signature}?cluster=devnet`)
     } catch (e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
